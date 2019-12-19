2932f740c4dc1e294c251f72a5cdaa9e7834df90
Ivan Krutov <vania-pooh@vania-pooh.com>
2019-12-19 11:17:32 +0300

    Updated webdriver.io to version that works properly

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     runner: 'local',
     protocol: 'https',
-    host: 'browsers.aerokube.com',
+    hostname: 'browsers.aerokube.com',
     port: 4444,
     user: '', // Username
     key: '', // Password
